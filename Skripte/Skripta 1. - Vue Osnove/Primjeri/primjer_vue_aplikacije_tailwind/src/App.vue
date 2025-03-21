e10e4c6342800351021469583239e665ced038f4
Alesandro Zuzic <azuzic@student.unipu.hr>
2025-03-21 17:50:29 +0100

    Skripta 2 fix

--- Skripte/Skripta 1. - Vue Osnove/Primjeri/primjer_vue_aplikacije_tailwind/src/App.vue
+++ Skripte/Skripta 1. - Vue Osnove/Primjeri/primjer_vue_aplikacije_tailwind/src/App.vue
@@ -1,11 +1,45 @@
 <script setup>
+
 </script>
 
 <template>
-    <div class="bg-gradient-to-br from-indigo-950 to-gray-950 h-full flex justify-center items-center">
-        <span class="transition-all duration-300 ease-in-out text-gray-100 font-mono text-6xl font-bold
-            hover:text-FIPU-blue hover:scale-125 hover:tracking-[0.25rem]">
-            Hello, World!
-        </span>
-    </div>
+
+<div class="w-screen h-screen bg-gradient-to-br from-emerald-950 to-indigo-950 text-teal-100 text-center">
+
+<div class="text-nowrap pt-4 pb-4 text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+  Pozdrav, ja sam <span class="font-bold text-cyan-400">Mario Bariša</span>
+</div>
+<hr class="w-5xl opacity-75 m-auto border-2 rounded-full"> 
+<div class="text-teal-100/75 p-4 pl-195 pt-12 text-left text-base sm:text-lg md:text-xl">
+  Moji omiljeni filmovi:
+</div>
+
+<div class="flex flex-col sm:flex-row gap-10 pt-14 w-fit rounded-b-xl justify-center m-auto mt-5">
+
+  <div class="flex flex-col items-center overflow-hidden">
+    <img class="object-fill w-[320px] h-[480px] border-0 border-opacity-0 rounded-[6px]"
+      src="https://m.media-amazon.com/images/M/MV5BMTg3Njc2ODEyN15BMl5BanBnXkFtZTcwNTAwMzc3NA@@._V1_FMjpg_UX654_.jpg">
+    <span class="pt-2 text-sm sm:text-base md:text-lg">Operacija Valkira</span>
+  </div>
+
+  <div class="flex flex-col items-center">
+    <img class="object-fill w-[320px] h-[480px] border-0 border-opacity-0 rounded-[6px]"
+      src="https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UY3600_.jpg">
+    <span class="pt-2 text-sm sm:text-base md:text-lg">Interstellar</span>
+  </div>
+
+  <div class="flex flex-col items-center">
+    <img class="object-fill w-[320px] h-[480px] border-0 border-opacity-0 rounded-[6px]"
+      src="https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_QL75_UX380_CR0,0,380,562_.jpg">
+    <span class="pt-2 text-sm sm:text-base md:text-lg">Star Wars</span>
+  </div>
+
+</div>
+
+</div>
+
+
+
 </template>
+
+<style scoped></style>
